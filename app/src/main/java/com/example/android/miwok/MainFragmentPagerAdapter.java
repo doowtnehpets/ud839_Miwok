67ab8e2231ea3219afb7c1def165c30ca8325c71
Stephen Wood <doowtnehpets@gmail.com>
2018-06-29 20:42:30 -0600

    Lesson 10 - part 12. Changed to FragmentViewPager. User slides between lists now.

---
+++ app/src/main/java/com/example/android/miwok/MainFragmentPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.android.miwok;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
+
+
+    public MainFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return new NumbersFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
